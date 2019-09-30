be469a0923cea565fb288d12a436ad567217d80d
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-09-30 14:02:21 +0200

    Add db sql script

---
+++ backend/scripts/db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `users` (
+	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+	`username` VARCHAR NOT NULL UNIQUE,
+	`firstname` VARCHAR,
+	`lastname` VARCHAR,
+	`digest_password` VARCHAR NOT NULL,
+	`level` INT NOT NULL
+);
+
+CREATE TABLE `messages` (
+	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+	`from` INTEGER NOT NULL,
+	`to` INTEGER NOT NULL,
+	`timestamp` INTEGER NOT NULL,
+	`subject` VARCHAR NOT NULL,
+	`message` VARCHAR NOT NULL,
+  	FOREIGN KEY(from) REFERENCES users(id),
+	FOREIGN KEY(to) REFERENCES users(id)
+);
