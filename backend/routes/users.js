7a0caa6d674dd4c6886dcfd0faa2cf3c58197ae1
Filipe Fortunato <filipe.fortunato@heig-vd.ch>
2019-10-08 13:00:42 +0200

    send message endpoint, for real, this time

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,7 +6,7 @@ const db = require('../middleware/db');
 /* GET all users. */
 router.get('/', function (req, res, next) {
 
-    const stmt = db.prepare('SELECT id, username, firstname, lastname, level, active FROM users' + req.role === 0 ? "WHERE active = 1" : "");
+    const stmt = db.prepare('SELECT id, username, firstname, lastname, level, active FROM users' + (req.role === 0 ? " WHERE active = 1" : ""));
     const rows = stmt.all();
     res.send(rows);
 });
